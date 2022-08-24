21183639101867ec8e1ffe62a8479c172e0cdc64
Exr0n <mail@exr0n.com>
2022-08-23 23:26:37 -0700

    graphin is soo much ez

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.png';
+import Graphin, { Utils } from '@antv/graphin';
+import { MiniMap } from '@antv/graphin-components';
+import { graphData } from './secret-data';
+
+//const data = Utils.mock(10).random().graphin();
+const data = graphData;
+
+function App() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+            <Graphin
+                data={data}
+                theme={{ mode: 'dark' }}
+            >
+              <MiniMap style={{ background: "000" }} />
+            </Graphin>
+        </div>
+    );
+}
+
+export default App;
+
